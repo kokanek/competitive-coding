c541f39bf39c4517ef0c41efbabb3e18e1c86419
Kapeel Kokane <kapeel.kokane@gmail.com>
2019-04-16 00:05:20 +0530

    two sum final

--- leetcode/two_sum_2.js
+++ leetcode/two_sum_2.js
@@ -37,3 +37,33 @@ var addTwoNumbers = function(l1, l2) {
     
     return result.next;
 };
+
+
+// solution with optimized carry
+var addTwoNumbers = function(l1, l2) {    
+    let ans = {val: 0, next: null};
+    let result = ans;
+    let carry = null;
+    
+    while(l1!==null || l2!==null || carry) {
+        let v1 = l1 ? l1.val : 0;
+        let v2 = l2 ? l2.val : 0;
+        let c = carry ? carry : 0;
+        
+        let sum = v1+v2+c;
+        if (carry) 
+            carry = null;
+        
+        if(sum > 9) {
+            ans.next = {val: sum%10, next: null};
+            carry = 1;
+        } else {
+            ans.next = {val: sum, next: null};
+        }
+        l1 = l1? l1.next: l1;
+        l2 = l2? l2.next: l2;
+        ans = ans.next;
+    }
+    
+    return result.next;
+};
